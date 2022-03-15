43673061e2a4e980fbb0718519a9d626ca1c12d3
kpaveliev <kpaveliev@gmail.com>
2022-03-15 19:50:45 +0300

    task 1 removed 2nd cycle by using get method

--- 01_cities_group.py
+++ 01_cities_group.py
@@ -9,11 +9,8 @@ cities_by_countries = {}
 
 for item in cities:
     country = item['fields']['country_ru']
-    cities_by_countries[country] = []
-
-for item in cities:
-    country = item['fields']['country_ru']
     city = item['fields']['name']
+    cities_by_countries[country] = cities_by_countries.get(country, [])
     cities_by_countries[country].append(city)
 
 # Printing results
@@ -21,7 +18,6 @@ for country in cities_by_countries:
     character = '\n- '
     print(
         f'{country}:\n'
-        f'- '
-        f'{character.join(cities_by_countries[country])}'
+        f'- {character.join(cities_by_countries[country])}'
     )
 
